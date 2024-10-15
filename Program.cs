195e539f30ff019d016f4947a1a831b36e67c990
Astra <vicanyuru@gmail.com>
2024-10-15 09:12:48 +0200

    new file:   Apple Game.csproj 	new file:   Apple Game.sln 	new file:   Program.cs 	new file:   bin/Debug/net8.0/Apple Game.deps.json 	new file:   bin/Debug/net8.0/Apple Game.dll 	new file:   bin/Debug/net8.0/Apple Game.exe 	new file:   bin/Debug/net8.0/Apple Game.pdb 	new file:   bin/Debug/net8.0/Apple Game.runtimeconfig.json 	new file:   obj/Apple Game.csproj.nuget.dgspec.json 	new file:   obj/Apple Game.csproj.nuget.g.props 	new file:   obj/Apple Game.csproj.nuget.g.targets 	new file:   obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	new file:   obj/Debug/net8.0/Apple Game.AssemblyInfo.cs 	new file:   obj/Debug/net8.0/Apple Game.AssemblyInfoInputs.cache 	new file:   obj/Debug/net8.0/Apple Game.GeneratedMSBuildEditorConfig.editorconfig 	new file:   obj/Debug/net8.0/Apple Game.GlobalUsings.g.cs 	new file:   obj/Debug/net8.0/Apple Game.assets.cache 	new file:   obj/Debug/net8.0/Apple Game.csproj.CoreCompileInputs.cache 	new file:   obj/Debug/net8.0/Apple Game.csproj.FileListAbsolute.txt 	new file:   obj/Debug/net8.0/Apple Game.dll 	new file:   obj/Debug/net8.0/Apple Game.genruntimeconfig.cache 	new file:   obj/Debug/net8.0/Apple Game.pdb 	new file:   obj/Debug/net8.0/apphost.exe 	new file:   obj/Debug/net8.0/ref/Apple Game.dll 	new file:   obj/Debug/net8.0/refint/Apple Game.dll 	new file:   obj/project.assets.json 	new file:   obj/project.nuget.cache

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+class SnakeGame
+{
+
+    static bool gameOver = false;
+    static int x = 5;
+    static int appleX = 10;
+    static int appleY = 1;
+    static int appleGenTimer = 0;
+    static int points = 0;
+    static int gamespeed = 10;
+    static void Main()
+    {
+        while (!gameOver)
+        {
+            if (Console.KeyAvailable)
+            {
+                ConsoleKeyInfo keyInfo = Console.ReadKey();
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.LeftArrow:
+                    if(x>0)
+                    {
+                        x--;   
+                    }
+                    break;
+                    case ConsoleKey.RightArrow:
+                    if(x<20)
+                    {    
+                        x++;
+                    }
+                    break;
+                    case ConsoleKey.Escape:
+                    EndGame();
+                    break;
+                }
+
+                
+            }
+
+            if (appleGenTimer == 40)
+            {
+                if (appleY==10)
+                {
+                    if (appleX >= x && appleX <= x + 4)
+                    {
+                        points++;
+                        appleY = 1;
+                        Random rng = new Random();
+                        appleX = rng.Next(0, 20);
+                    }
+                    else 
+                    {
+                        EndGame();
+                    }
+                }
+                appleY++;    
+                appleGenTimer = 0;
+            }
+
+            gamespeed = 200+points; //Increases Gamespeed over time
+            Console.Clear();
+            Console.WriteLine("Apple Catcher | Points: " + points);     //
+            Console.SetCursorPosition(appleX, appleY);                  //
+            Console.WriteLine("O");                                     //              Renders each frame
+            Console.SetCursorPosition(x, 10);                           // 
+            Console.WriteLine("\\___/");                                // 
+            Console.CursorVisible = false;                               //
+
+            appleGenTimer++;                                            
+            System.Threading.Thread.Sleep(1000/gamespeed);              //      makes the apple fall 1 space every 40000ms/gamespeed
+            
+        }
+    }
+    static void EndGame()
+    {
+        gameOver = true;
+    }
+}
